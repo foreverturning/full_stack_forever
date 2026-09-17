28e2a85dd0724d84d0eefc0f40a1eaacb3753dc0
foreverturning <foreverturning123@gmail.com>
2026-09-18 07:50:57 +0800

    fastapi 1

--- backend/main.py
+++ backend/main.py
@@ -1,32 +1,34 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import json
+from fastapi import FastAPI
+from pydantic import BaseModel # Pydantic，专门管理数据的解析和校验；BaseModel，声明某个类型的数据长什么样
+
+app = FastAPI()
 
 profile = {
     "heroTitle": "关于我",
     "heroSubtitle": "项目，创意，灵感，心得，我的作品",
 }
 
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
+class AnalyzeRequest(BaseModel):
+    # AnalyzeRequest的要求，按照下面的要求做校验
+    # 请求体校验不通过，请求方 会被打回
+    text: str # 要求提供一个叫做 text 的值，且这个值是 字符串 类型
 
-        # print(self.headers)          # 收到的请求头
-        # print(self.client_address)   # 请求是从哪个地址来的
-        if self.path == "/api/profile":
-            self.send_response(200)
-            self.send_header("Content-Type", "application/json")
-            self.end_headers()
-            body = json.dumps(profile, ensure_ascii=False)  # ensure_ascii=False：让中文原样输出
-            self.wfile.write(body.encode("utf-8"))
 
-        # elif self.path == "/hello":
-        #     self.send_response(200)
-        #     self.send_header("Content-Type", "text/html; charset=utf-8")   # ← 一会儿改成 text/plain 再试
-        #     self.end_headers()
-        #     self.wfile.write("<h1>你好，HTTP</h1>".encode("utf-8"))
+# fastapi部分：
 
-        else:
-            self.send_response(404)
-            self.end_headers()
+# 遇到不认识的路径，自动返回404
+@app.get("/api/profile") # 装饰器，向路径发的get请求，交给下面的函数处理
+def get_profile():
+    return profile
 
-print("后端已启动：http://localhost:8000/api/profile")
-HTTPServer(("", 8000), Handler).serve_forever()
+@app.post("/api/analyze") # 请求该地址是post请求，交给下面的函数处理
+def analyze(req: AnalyzeRequest): # 需要提供request(req)请求体，这个请求体的要求 ： 必须符合 AnalyzeRequest 的要求
+    return {
+        "text": req.text,
+        "score": 0.5,
+        "label": "偏平静",
+        "pinyin": "（模块 6 再说）",
+    }
+
+
+# 用uvicorn启动
